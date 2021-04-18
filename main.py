9a43d7deadae237b2e79826ddc10265e5c691cbf
Frederik Snedevind <fsnedevind@hotmail.com>
2021-04-18 19:37:44 +0200

    Made both sensors work on startup. Major tweaks. Changed topic names. Now publishing graphdata every boot with retain=True

--- main.py
+++ main.py
@@ -1,74 +1,81 @@
 
-from machine import Pin, I2C, reset
-from machine import deepsleep
-import schedule
-import utime
-from time import sleep
-import ujson
+from machine import Pin, I2C, reset, unique_id, deepsleep
+from ubinascii import hexlify
 from random import randint
+from utime import sleep
+import ujson
 import utils
-import ubinascii        # Used by the MQTT setup
-import simple           # Used by MQTT
-from machine import unique_id
+import simple                       # Used by MQTT
 import esp32
+import sensors
 
-#import sensors
-import ssd1306
+
+def publishGraph(interval=1):
+    temp = {}
+
+    with open ('data.json', 'r') as dataFile:
+        temp = ujson.load(dataFile)
+
+    data = {}
+    temperature = []
+    humidity = []
+    height = []
+
+    samplesTaken = 0
+    i = 0
+
+    for a in range(0, len(temp['temperature'])):
+        try:
+            if i % interval == 0:
+                temperature.append(temp['temperature'][i])
+                humidity.append(temp['humidity'][i])
+                height.append(temp['height'][i])
+                samplesTaken += 1
+            i += 1
+        except:
+            print ("exception!!")
+
+    for a in range(20 - len(temp['temperature'])):
+        temperature.insert(0, 0)
+        humidity.insert(0,0)
+        height.insert(0,0)
+
+    data['temperature'] = temperature
+    data['humidity'] = humidity
+    data['height'] = height
+
+    client.publish(b'/sourSmart/out/graphData', str(data).encode(), retain=True)
+    print ("Published graphdata:", data)
+
+
 
 
 def callbackFunction(topic, msg):
+    global client
+
     msg = msg.decode('utf-8')
     topic = topic.decode('utf-8')
 
     print ("Received:", msg, "on topic:", topic)
     ### If we received a command, handle it here:
-    if topic == '/command':
+    if topic == '/sourSmart/in/command':
         if msg == 'ping':
-            client.publish(b'/response', b'pong')
+            client.publish(b'/sourSmart/out/response', b'pong')
         
         elif msg == 'restart':
-            client.publish(b'/response', b'restarting in 5...')
+            client.publish(b'/sourSmart/out/response', b'restarting in 5...')
             sleep(5)
             reset()
 
+        elif msg == 'timer':
+            client.publish(b'/sourSmart/out/response',b'Time to sleep: ' + str(timer))
 
     ### If we received a setGraphInterval request, handle it here
-    elif topic == '/setGraphInterval':
-        interval = int(msg)
-        temp = {}
-
-        with open ('data.json', 'r') as dataFile:
-            temp = ujson.load(dataFile)
-
-        data = {}
-        temperature = []
-        humidity = []
-        height = []
-
-        print ("Length of dataset:", len(temp['temperature']))
-        print ("Interval is",interval)
-        sampleLength = int(len(temp['temperature']))/interval
-
-        print ("Samplelength: ", sampleLength)
-
-        for b in range(0, len(temp['temperature']), interval):
-            print ("b:",b)
-
-
-
-        for a in range(20):
-            temperature.append(temp['temperature'][-1-(interval*a)])
-            humidity.append(temp['humidity'][-1-(interval*a)])
-            height.append(temp['height'][-1-(interval*a)])
-
-        data['temperature'] = temperature[::-1]
-        data['humidity'] = humidity[::-1]
-        data['height'] = height[::-1]
+    elif topic == '/sourSmart/in/setGraphInterval':
         
-        client.publish(b'/graphData', str(data).encode())
-        print ("Published", data)
+        publishGraph(interval=int(msg))
 
-    elif topic == '/toggleFeed':
+    elif topic == '/sourSmart/in/toggleFeed':
         temp = {}
 
         with open ('data.json', 'r') as dataFile:
@@ -78,7 +85,7 @@ def callbackFunction(topic, msg):
         response['time'] = msg
         response['currentHeight'] = temp['height'][-1]
 
-        client.publish(b'/feedData', str(response).encode())
+        client.publish(b'/sourSmart/out/feedData', str(response).encode())
         
     else:
         print ("Received unhandled topic message", topic, msg)
@@ -94,13 +101,6 @@ def buttonJob(buttonInput):
 
 
 
-
-### Button setup
-buttonInput = Pin(35, Pin.IN)
-buttonInput.irq(trigger=Pin.IRQ_FALLING, handler=buttonJob)
-esp32.wake_on_ext0(pin = buttonInput, level = esp32.WAKEUP_ANY_HIGH)
-
-
 def restart_and_reconnect():
   print('Failed to connect to MQTT broker. Reconnecting...')
   sleep(10)
@@ -109,35 +109,51 @@ def restart_and_reconnect():
 
 def mqttSetup():
     mqtt_server = '135.181.193.6'
-    client_id = ubinascii.hexlify(unique_id())
-    subscribeTopics = [ b'/command',
-                        b'/setGraphInterval',
-                        b'/toggleFeed']
-
+    client_id = hexlify(unique_id())
+    topic = '/sourSmart/in/#'
     client = simple.MQTTClient(client_id, mqtt_server)
     client.set_callback(callbackFunction)
     client.connect()
-    for a in subscribeTopics:
-        client.subscribe(a)
+
+    try:
+        client.subscribe(topic.encode('utf-8'))
+        print ("Subscribed to", topic)
+    except:
+        print ("Couldn't subscribe to", topic)
+        restart_and_reconnect()
 
     print('Connected to %s MQTT broker' % (mqtt_server))
     return client
 
 
 def goToSleep(minutes=1):
-    client.publish(b'/response',b'Sleeping for', minutes, 'minutes')
-    sleep(1)
-    deepsleep(minutes*60*1000)
+    sleepString = 'Sleeping for' + str(minutes) + 'minutes'
+    client.publish(b'/sourSmart/out/response',sleepString.encode('utf-8'))
+
+    sleep(2)
+
+    deepsleep(minutes * 60 * 1000)
 
 
+##### Bootup
+### Button setup
+buttonInput = Pin(35, Pin.IN)
+buttonInput.irq(trigger=Pin.IRQ_FALLING, handler=buttonJob)
+esp32.wake_on_ext0(pin = buttonInput, level = esp32.WAKEUP_ALL_LOW)
 
-
-
-### Bootup
+### Wifi connect
 utils.connectWifi('CableBox-BF58','ymn5gzm5um')
-client = mqttSetup()
-client.publish(b'/response', b'Goodmorning!')
 
+### MQTT Client setup
+client = mqttSetup()
+
+### Collect a data point and publish it
+sensors.sensorCollectionJob()
+
+### Publish current graphs
+publishGraph(1)
+
+### Set a timer to stay alive for 60 seconds
 timer = 60
 period = 0.5
 
@@ -149,13 +165,7 @@ while True:
         if timer % 5 == 0:
             print ("Time to sleep:", timer)
         if timer <= 0:
-            break
-
+            goToSleep(minutes=15)
 
     except OSError as e:
         restart_and_reconnect()
-
-
-goToSleep(minutes=15)
-
-
--- sensors.py
+++ sensors.py
@@ -1,36 +1,102 @@
-
-from machine import Pin, Timer, I2C, SoftI2C
+from machine import Pin, Timer, SoftI2C
 import ujson
 from random import randint
 import ahtx0
+from utime import sleep
+import VL5XLX
 
 ##### Sensor initialization
-
-# setup pins for sensors
 i2c = SoftI2C(scl=Pin(22), sda=Pin(21))
-sensor = ahtx0.AHT10(i2c)
 
-## setup timer for sensors
-sensorCollectionTimer = Timer(0)
-sensorCollectionTimer.init(period=5000, mode=Timer.PERIODIC, callback=sensorCollectionJob)
+envSensor = None
+tofSensor = None
 
-def sensorCollectionJob(sensorCollectionTimer):
-    global sensor
+## Setup power-pins for the 2 sensors
+envSensorPower = Pin(12, Pin.OUT)
+tofSensorPower = Pin(13, Pin.OUT)
 
-    print ("Temperature:", sensor.temperature)
-    print ("Humidity:", sensor.relative_humidity)
-    data = {}
+## Zero both powers
+envSensorPower.value(0)
+tofSensorPower.value(0)
+
+## Init setup of env sensor
+try:
+    print ("Setting up envSensor...")
+    envSensorPower.value(1)
+    sleep(0.5)
+    envSensor = ahtx0.AHT10(i2c)
+    print ("envSensor setup success!")
+
+except:
+    print ("Failed setting up envSensor")
+    envSensorPower.value(0)
     
-    data['temp'] = randint(15, 35)
-    data['humid'] = randint(10, 90)
-    data['height'] = randint(0, 100)
+try:
+    print ("Setting up ToF Sensor...")
+    tofSensorPower.value(1)
+    sleep(0.5)
+    tofSensor = VL5XLX.VL53L1X(i2c)
+    envSensorPower.value(1)
+    print ("ToF Sensor setup success!")
+except:
+    print("Failed setting up ToFSensor")
+    tofSensorPower.value(0)
+    print (tofSensor)
+
+def sensorCollectionJob():
+    
+    data = {}
+    if envSensor != None:
+        data['temperature'] = envSensor.temperature
+        data['humidity'] = envSensor.relative_humidity
+
+    else:
+        data['temperature'] = randint(15, 35)
+        data['humidity'] = randint(10, 90)
+    
+    if tofSensor != None:
+        data['height'] = (int(tofSensor.read()) / 10)
+    else:
+        data['height'] = randint(0, 100)
 
     writeSensorData(data)
 
 def writeSensorData(dataDict):
     with open ('data.json' , 'r') as jsonFile:
         data = ujson.load(jsonFile)
-        data['data'].append(dataDict)
+        
+        ### If there's >= 80 datapoints, delete the oldest ones
+    if len(data['temperature']) >= 80:
+        dataOverflowAmount = len(data['temperature']) - 79
+
+        print ("Deleting", dataOverflowAmount, "datapoints")
+
+        for a in range(dataOverflowAmount):
+            del data['temperature'][0]
+            del data['humidity'][0]
+            del data['height'][0]
+
+    data['temperature'].append(dataDict['temperature'])
+    data['humidity'].append(dataDict['humidity'])
+    data['height'].append(dataDict['height'])
 
     with open ('data.json', 'w') as jsonFile:
+        ujson.dump(data, jsonFile)
+
+
+def printDataLib():
+    with open ('data.json', 'r') as jsonFile:
+        data = ujson.load(jsonFile)
+        for a in data:
+            print (a, data[a])
+
+
+def deleteData():
+    with open('data.json', 'w') as jsonFile:
+        data = {}
+        
+        data['temperature'] = []
+        data['humidity'] = []
+        data['height'] = []
+
         ujson.dump(data, jsonFile)
